14b19e496465963c1c47267b6f6c6d1bfb5918c8
Daniel Medeiros <daniel@arneam.com>
2019-10-04 05:27:31 -0300

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gists-bkp
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/gists_bkp/core_test.clj
@@ -0,0 +1,7 @@
+(ns gists-bkp.core-test
+  (:require [clojure.test :refer :all]
+            [gists-bkp.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
