eb5ca6099ab220becdd9ae2c68383a1b2d12132c
Daniel Medeiros <daniel@arneam.com>
2019-10-06 06:00:10 -0300

    Add function to get all public Github gists

--- src/gists_bkp/core.clj
+++ src/gists_bkp/core.clj
@@ -1,6 +1,17 @@
-(ns gists-bkp.core)
+(ns gists-bkp.core
+  (:require [clj-http.client :as client]
+            [cheshire.core :refer :all]))
+
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn get-gists-pull-url
+  []
+  (parse-string (:body (client/get "https://api.github.com/users/medeiros/gists")) (fn [x] (= x "git_pull_url")))
+  ;; iterate through all items: (val (last (item x)))
+  ;; then create own list of gist urls
+  
+  )
