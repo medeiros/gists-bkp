2fe904d6a4b217ef6c6542b0534442bebfc517e1
Daniel Medeiros <daniel@arneam.com>
2019-10-07 05:24:55 -0300

    Correct method to iterate through public gists
    The previous implementation was not working properly. Now, the method is
    printing the git url as expected. It will be improved, now, to backup
    the code related to each one of those urls.

--- src/gists_bkp/core.clj
+++ src/gists_bkp/core.clj
@@ -8,10 +8,17 @@
   [x]
   (println x "Hello, World!"))
 
+
+(def gists_url "https://api.github.com/users/medeiros/gists")
+
 (defn get-gists-pull-url
+  "Iterate through all public gists"
   []
-  (parse-string (:body (client/get "https://api.github.com/users/medeiros/gists")) (fn [x] (= x "git_pull_url")))
-  ;; iterate through all items: (val (last (item x)))
-  ;; then create own list of gist urls
-  
-  )
+  (let [lseq (parse-string (:body (client/get gists_url)) 
+                          (fn [x] (= x "git_pull_url")))]
+    (loop [item (first lseq), sublist (rest lseq)]
+      (when item
+        (println (val (last item)))
+        (recur (first sublist) (rest sublist))))))
+
+
