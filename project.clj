dd760cb400a1c904eb3e5ceca531ae8ea1f322ff
Daniel Medeiros <daniel@arneam.com>
2019-10-08 04:52:52 -0300

    Add clj-jgit dependency to clone gists repos

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "3.10.0"]
-                 [cheshire "5.9.0"]])
+                 [cheshire "5.9.0"]
+                 [clj-jgit "1.0.0-beta2"]])
