5d7083338f7a06dd1b278c8e97c30fd8c17a55d2
Daniel Medeiros <daniel@arneam.com>
2019-10-09 05:11:26 -0300

    Extract clone message in its own function

--- src/gists_bkp/core.clj
+++ src/gists_bkp/core.clj
@@ -12,10 +12,18 @@
 
 (def gists_url "https://api.github.com/users/medeiros/gists")
 
+
 (defn get-file-from-uri
   "Get a file part from a uri."
   [uri]
   (subs uri (inc (clojure.string/last-index-of uri "/"))))
+
+
+(defn clone-message
+  "Returns cloning action message."
+  [url file]
+  (str "cloning " url " (file: " file ") ..."))
+
 
 (defn get-gists-pull-url
   "Iterate through all public gists and clone those repos.
@@ -27,7 +35,7 @@
       (when item
         (let [url (val (last item))
               file (get-file-from-uri url)]
-          (println (str "cloning " url " (file: " file ") ..."))
+          (println (clone-message url file))
           (jgit/git-clone url :dir (str "gist-clones/" file)))
         (recur (first sublist) (rest sublist))))))
 
--- test/gists_bkp/core_test.clj
+++ test/gists_bkp/core_test.clj
@@ -6,4 +6,9 @@
   (testing "URI file name is incorrect"
     (is (= (get-file-from-uri "https://gist.github.com/medeiros/12345.git") "12345.git"))))
 
+(deftest should-get-cloning-message
+  (testing "Log action not working as expected"
+    (is (= (clone-message "https://gists.github.com/medeiros/12345.git" "12345.git") 
+           "cloning https://gists.github.com/medeiros/12345.git (file: 12345.git) ..."))))
+
 
